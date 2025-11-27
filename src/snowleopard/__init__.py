ccc8099ad02fd92fa16beee1e721017ce0123363
chillipino <chillipino@users.noreply.github.com>
2025-11-26 16:38:30 -0800

    snowleopard_py: bump to version 0.1.1

--- src/snowleopard/__init__.py
+++ src/snowleopard/__init__.py
@@ -5,7 +5,7 @@
 from snowleopard.async_client import AsyncSnowLeopardPlaygroundClient
 from snowleopard.client import SnowLeopardPlaygroundClient
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "SnowLeopardPlaygroundClient",
